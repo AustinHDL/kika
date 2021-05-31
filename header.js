31a2477cd726b2c6a100cfaaa9607465c49c07d4
AustinHDL <72658046+AustinHDL@users.noreply.github.com>
2021-05-31 21:36:21 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,19 @@
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => {
+	selectElement('header').classList.toggle('active');
+	selectElement('body').classList.toggle('lock');
+
+});
+
+selectElement('#list-0').addEventListener('click', () => {
+	document.getElementById('footer-list-0').classList.toggle('showlist0');
+	document.getElementById('footer-column').classList.toggle('rotateicon');
+
+});
+
+selectElement('#list-1').addEventListener('click', () => {
+	document.getElementById('footer-list-1').classList.toggle('showlist1');
+	document.getElementById('footer-column-1').classList.toggle('rotateicon');
+
+});
